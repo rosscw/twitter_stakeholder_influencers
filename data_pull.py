78145c555ce0b4fd37488a9762701c8affe1658c
rosscw <77053161+rosscw@users.noreply.github.com>
2021-12-19 14:25:52 +0000

    Update data_pull.py
    Set up to pull auth_tokens from other file

--- data_pull.py
+++ data_pull.py
@@ -1,11 +1,15 @@
 # Set up
 pip install tweepy
 import tweepy
+from auth_tokens import * 
 
-Auth details:
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-
+# Define authorised client call
 client = tweepy.Client(bearer_token = bearer_token, consumer_key = consumer_key, consumer_secret = consumer_secret)
 
-for id in tweepy.Paginator(client.get_users_following, id = 515600557, max_results = 100).flatten(limit = 5) :
+# Extract handles for each account being followed by stk_id
+stk_id = 515600557 #tara_flores as test case
+for id in tweepy.Paginator(client.get_users_following, id = stk_id, max_results = 100).flatten(limit = 5) :
                            print(id)
