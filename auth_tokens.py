8005c432bb040834d558a0b55f0cd51f5edada10
rosscw <77053161+rosscw@users.noreply.github.com>
2021-12-19 14:22:30 +0000

    Create auth_tokens.py

---
+++ auth_tokens.py
@@ -0,0 +1,4 @@
+# Tokens
+bearer_token = "AAAAAAAAAAAAAAAAAAAAACPOXAEAAAAAkz879L%2FFC%2B7e1hPmAFeSFor1AT0%3DA3zJhVqvMCBelUzLOyQ6K086HQqo5hsRWYtHJJC2DbOlkiElxx"
+consumer_key = "ua4jrmNNStVQdMXAFYUgeoiiP"
+consumer_secret = "s5zUdmxlV9mDbXJyt0RwaXIrrPMuZAEMz2csc6dOLDpCxRSzh4"
